8c00b7136b3c96cc7fa34e6779f0d8da0c378638
민규 김 <codemin9@gmail.com>
2024-03-12 17:03:49 +0900

    클래스 상속을 통한 윈도우

---
+++ TestForm/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    class MyForm : System.Windows.Forms.Form
+    {
+
+    }
+    internal class MainApp : System.Windows.Forms.Form
+
+
+    {
+        static void Main(string[] args)
+        {
+            MyForm form = new MyForm();
+            form.Click += new EventHandler(
+                (sender, eventArgs) =>
+                {
+                    Application.Exit();
+                });
+            Application.Run(form);
+
+        }
+    }
+}
