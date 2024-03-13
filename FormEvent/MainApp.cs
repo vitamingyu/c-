b325fe6cadaab5f01e248b5386ec2a103312676f
민규 김 <codemin9@gmail.com>
2024-03-13 09:12:11 +0900

    폼 이벤트_클릭시 메시지 출력

--- FormEvent/MainApp.cs
+++ FormEvent/MainApp.cs
@@ -3,11 +3,26 @@ using System.Windows.Forms;
 
 namespace FormEvent
 {
-    internal class MainApp
+    internal class MainApp:Form
     {
+        public void MyMouseHandler(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine($"Sender : {((Form)sender).Text}");
+            Console.WriteLine($"X:{e.X}, Y:{e.Y}");
+            Console.WriteLine($"Button:{e.Button}, Clicks:{e.Clicks}");
+            Console.WriteLine();
+        }
+
+        public MainApp(string title)
+        {
+            this.Text = title;
+            this.MouseDown +=
+                new MouseEventHandler(MyMouseHandler);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Application.Run(new MainApp("Mouse Event Test"));
         }
     }
 }
