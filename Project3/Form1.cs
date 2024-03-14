1921dd3d84b22f7140c94dcadb9a48d3df11d555
민규 김 <codemin9@gmail.com>
2024-03-14 18:07:50 +0900

    캔슬취소가 안됨

--- Project3/Form1.cs
+++ Project3/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms.VisualStyles;
 
 
 namespace Project3
@@ -19,10 +21,9 @@ namespace Project3
 
         public void fThreadStart()
         {
-            //    _thread = new Thread(new ThreadStart(Run));  // ThreadStart 델리게이트 타입 객체를 생성 후 함수를 넘김
             _thread = new Thread(Run);   // 컴파일러에서 델리게이트 객체를 추론해서 생성 후 함수를 넘김 (new~생략)
-
             _thread.Start();
+            btnExport.Enabled = false;
         }
 
         private void Run()
@@ -36,59 +37,70 @@ namespace Project3
                 bool rowResult = ulong.TryParse(row, out rowNumber);
                 bool colResult = ulong.TryParse(col, out colNumber);
 
-
                 string filepath = txtPath.Text;
-                string fullfilepath = Path.Combine(filepath, @"test.csv");
 
-                StreamWriter sw = new StreamWriter(fullfilepath);
-
+                StreamWriter sw = new StreamWriter(filepath);
 
                 sw.WriteLine("Hello World!!");
                 sw.WriteLine("csvFile. time 3sec");
                 Thread.Sleep(3000);
                 sw.Close();
 
-                if (!rowResult || !colResult)
 
+                if (!rowResult || !colResult)
                 {
                     MessageBox.Show("ROW와 COLUMN은 양의 정수값만 입력 가능합니다.", "오류발생", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-
                 }
                 else
                 {
-                    MessageBox.Show(fullfilepath + "에 파일이 성공적으로 저장되었습니다.");
+                    MessageBox.Show(filepath + "에 파일이 성공적으로 저장되었습니다.");
                 }
             }
 
             catch (Exception er)
             {
-                Console.WriteLine("Exception: " + er.Message);
+                er.ToString();
             }
+
         }
 
         public Form1()
         {
             InitializeComponent();
-            // btnlock();
         }
 
         private void btnPath_Click(object sender, EventArgs e)
         {
+            //string folderPath = "";
+            //FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
+            //if (directchoosedlg.ShowDialog() == DialogResult.OK)
+            //{
+            //    folderPath = directchoosedlg.SelectedPath;
+            //    txtPath.Text = folderPath;
+            //}
+
             string folderPath = "";
-            FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
-            if (directchoosedlg.ShowDialog() == DialogResult.OK)
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV File|*.csv";
+            saveFileDialog.Title = "Save an csv file";
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.DefaultExt = "csv";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                folderPath = directchoosedlg.SelectedPath;
+                folderPath = saveFileDialog.FileName;
                 txtPath.Text = folderPath;
+
             }
         }
 
         private void btnExport_Click(object sender, EventArgs e)
         {
-            _thread = new Thread(Run);   // 컴파일러에서 델리게이트 객체를 추론해서 생성 후 함수를 넘김 (new~생략)
 
-            _thread.Start();
-
+            fThreadStart();
             /*
              int ibar = 0;
              if(bar.Value + ivar > 100)
@@ -102,18 +114,17 @@ namespace Project3
             //            lblpro.Text = string.Format("{0}%", bar.value);
         }
 
+        public void ThreadAbort()
+        {
+            if (_thread != null && _thread.IsAlive)
+            {
+                _thread.Abort();
+            }
+        }
 
-
-
-
-        //public void ThreadAbort()
-        //{
-        //    if (_thread.IsAlive)
-        //    {
-        //        _thread.Abort();
-        //        btnExport.Enabled = false;
-        //    }
-
-        //}
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ThreadAbort();
+        }
     }
 }
