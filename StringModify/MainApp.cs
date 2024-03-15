a67ba0350b1014a9a37ad6e7a469afc76b9c51c9
민규 김 <codemin9@gmail.com>
2024-03-15 16:47:08 +0900

    문자열 변형(trim,insert,remove)

---
+++ StringModify/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace StringModify
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Insert() : '{0}'", "Happy Friday!".Insert(5, " Sunny"));
+            WriteLine("Remove() : '{0}'", "I Don't Love You".Remove(2, 6));
+
+            WriteLine("Trim() : '{0}'", " No Spaces ".Trim());
+            WriteLine("TrimStart() : '{0}'", " No Spaces ".TrimStart());
+            WriteLine("TrimEnd() : '{0}'", " No Spaces ".TrimEnd());
+        }
+    }
+}
