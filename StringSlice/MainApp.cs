1ad4482530c033a2a84b52571a67c2e6aab6e690
민규 김 <codemin9@gmail.com>
2024-03-15 16:57:02 +0900

    substring, split

---
+++ StringSlice/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace StringSlice
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good morning.";
+
+            WriteLine(greeting.Substring(0,5));  //Good
+            WriteLine(greeting.Substring(5)); // 5부터 끝까지
+            WriteLine();
+
+            string[] arr = greeting.Split(
+                new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+                WriteLine("{0}", element);
+        }
+    }
+}
