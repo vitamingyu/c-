439a12454ec221fe49763827fb15a115d1398a5a
민규 김 <codemin9@gmail.com>
2024-03-22 15:12:23 +0900

    접근지정자(예외처리가 안됨)

---
+++ AccessModifier/Mainapp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class WaterHeater
+    {
+        protected int temperature;
+
+        public void SetTemperature(int temmperature)
+        {
+            if(temperature < -5 || temperature > 42)
+            {
+                throw new Exception("Out of temperature range");
+            }
+            this.temperature = temmperature;
+        }
+
+        internal void TurnOnWater()
+        {
+            Console.WriteLine($"Turn on water : {temperature}");
+        }
+    }
+
+    class Mainapp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WaterHeater heater = new WaterHeater();
+                heater.SetTemperature(20);
+                heater.TurnOnWater();
+
+                heater.SetTemperature(-2);
+                heater.TurnOnWater();
+
+                heater.SetTemperature(50);
+                heater.TurnOnWater();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
