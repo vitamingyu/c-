052e33f9821f828505be1e86dca0d84885cbfd3b
민규 김 <codemin9@gmail.com>
2024-03-25 13:33:37 +0900

    오버라이딩 아이언맨 예시

---
+++ Overriding/MainApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Overriding
+{
+    class ArmorSuit
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("방어모드 실행, 갑바 장착");
+        }
+    }
+
+    class IronMan : ArmorSuit
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("아이언맨 수트 장착");
+        }
+    }
+
+    class WarMachine : ArmorSuit
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("무기 장착");
+            Console.WriteLine("로켓펀치 장착");
+        }
+    }
+
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating AromorSuit...");
+            ArmorSuit armorSuit = new ArmorSuit();
+            armorSuit.Initialize();
+
+            Console.WriteLine("\nCreating IronMan...");
+            ArmorSuit ironman = new IronMan();
+            ironman.Initialize();
+
+            Console.WriteLine("\nCreating WarMachine...");
+            ArmorSuit warmachine = new WarMachine();
+            warmachine.Initialize();
+        }
+    }
+}
