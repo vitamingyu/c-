930f757931c81204cc0b2b805c2a8956efe6ede5
민규 김 <codemin9@gmail.com>
2024-03-25 14:36:35 +0900

    메서드 봉인

---
+++ SealedMethod/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Base
+{
+    public virtual void SealMe()
+    {
+    }
+
+    class Derived : Base
+    {
+        public sealed override void SealMe()
+        {
+        }
+    }
+
+    class Ob : Derived
+    {
+        public override void SealMe()
+        {
+            // 위에서 봉인했기 때문에 여기서 오류남
+        }
+    }
+
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
