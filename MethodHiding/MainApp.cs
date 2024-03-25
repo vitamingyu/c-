1e815f95d041f5f707453d92e010909ff470cdf8
민규 김 <codemin9@gmail.com>
2024-03-25 14:25:14 +0900

    메서드 숨기기

---
+++ MethodHiding/MainApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+
+namespace MethodHiding
+{
+    class Base
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Base.MyMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        public new void MyMethod()
+        {
+            Console.WriteLine("Derived.MyMethod()");
+        }
+    }
+    internal class MainApp
+    {
+
+        static void Main(string[] args)
+        {
+            Base baseObj = new Base();
+            baseObj.MyMethod();
+
+            Derived derivedObj = new Derived();
+            derivedObj.MyMethod();
+
+            Base baseOrDerived = new Derived();
+            baseOrDerived.MyMethod();
+        }
+    }
+}
