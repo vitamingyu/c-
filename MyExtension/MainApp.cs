d6d7a01688e64ec68e6b4ddd731c70d16d075f3c
민규 김 <codemin9@gmail.com>
2024-03-25 17:19:25 +0900

    확장메소드

---
+++ MyExtension/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyExtension;
+
+namespace MyExtension
+{
+    public static class StringExtension
+    {
+        public static string Append(this string sr, string sr2)
+        {
+            return sr + sr2;
+        }
+    }
+}
+
+
+namespace ExtensionMethod
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            Console.WriteLine(hello.Append(", World!"));
+        }
+    }
+}
