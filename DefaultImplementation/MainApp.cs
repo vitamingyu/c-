d588a48b3fb6f3c4693172c291b2b374f5d23077
민규 김 <codemin9@gmail.com>
2024-03-26 15:54:12 +0900

    런타임에러, 기본구현 인터페이스

---
+++ DefaultImplementation/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DefaultImplementation
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+
+        void WriteError(string error)
+        {
+            WriteLog($"Error: {error}");
+        }
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now.ToLocalTime()}, {message}");
+        }
+    }
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.WriteLog("System Up");
+            logger.WriteError("System Fail");
+
+            ConsoleLogger clogger = new ConsoleLogger();
+            clogger.WriteLog("System Up");
+            // clogger.WriteError("System Fail");
+        }
+    }
+}
