ffc3164902ceca857a22c1440a0cb5514b542029
민규 김 <codemin9@gmail.com>
2024-03-27 16:00:04 +0900

    인터페이스의 프로퍼티

---
+++ PropertiesInInterface/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using PropertiesInInterface;
+using System;
+
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    {
+        string Name { get; set; }
+        string Value { get; set; }
+    }
+}
+
+class NamedValue : INamedValue
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        NamedValue name = new NamedValue()
+        {Name="이름", Value="박상현"};
+
+        NamedValue height = new NamedValue()
+        { Name = "키", Value = "177cm" };
+
+        NamedValue weight = new NamedValue()
+        { Name = "몸무게", Value = "90kg" };
+
+        Console.WriteLine($"{name.Name} : {name.Value}");
+        Console.WriteLine($"{height.Name} : {height.Value}");
+        Console.WriteLine($"{weight.Name} : {weight.Value}");
+
+    }
+}
