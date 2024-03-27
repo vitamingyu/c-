dc95fa87f88e5d943e942a29e91171bc671cd86b
민규 김 <codemin9@gmail.com>
2024-03-27 16:17:42 +0900

    추상프로퍼티 예제

---
+++ PropertiesInAbstractClass/MainApp.cs
@@ -0,0 +1,48 @@
+﻿using PropertiesInAbstractClass;
+using System;
+
+namespace PropertiesInAbstractClass
+{
+    abstract class Product
+    {
+        private static int serial = 0;
+        public string SerialId
+        {
+            get { return String.Format("{0:d5}", serial++); }
+        }
+
+        abstract public DateTime ProductDate
+        {
+            get; set;
+        }
+    }
+
+    class MyProduct : Product
+    {
+        public override DateTime ProductDate { get; set; }
+    }
+}
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Product product_1 = new MyProduct()
+        {
+            ProductDate = new DateTime(2023, 1, 10)
+        };
+
+        Console.WriteLine("Product:{0}, Product Date:{1}",
+            product_1.SerialId,
+            product_1.ProductDate);
+
+        Product product_2 = new MyProduct()
+        {
+            ProductDate = new DateTime(2023, 2, 3)
+        };
+
+        Console.WriteLine("Product:{0}, Product Date : {1}",
+            product_2.SerialId,
+            product_2.ProductDate);
+    }
+}
