7277b49237b45471a6524de7ef743025fd9a2e61
민규 김 <codemin9@gmail.com>
2024-03-28 14:56:06 +0900

    array배열 초기화하는 3가지 방법

--- ArraySample2/Properties/AssemblyInfo.cs
+++ ArraySample2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("ArraySample2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("ArraySample2")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("d0830c55-44b4-4342-bd4c-80e4c5ab51a9")]
+[assembly: Guid("b34c60e3-a19c-4066-9ef9-4e71e425f05c")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
