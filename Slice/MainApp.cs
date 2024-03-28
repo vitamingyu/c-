265b78831bfde0f2c45dc5f2f714a7aad6e837f9
민규 김 <codemin9@gmail.com>
2024-03-28 16:23:45 +0900

    slice ^

---
+++ Slice/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Slice
+{
+    internal class MainApp
+    {
+        static void PrintArray(System.Array array)
+        {
+            foreach (var e in array)
+                Console.Write(e);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            char[] array = new char['Z' - 'A' + 1];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = (char)('A' + i);
+
+            PrintArray(array[..]);  // 0번째부터 마지막까지
+            PrintArray(array[5..]);  // 5번째부터 끝까지
+
+            Range range_5_10 = 5..10;
+            PrintArray(array[range_5_10]);  // 5번째부터 9(10-1)번째까지
+
+            Index last = ^0;
+            Range range_5_last = 5..last;
+            PrintArray(array[range_5_last]);  // 5번째부터 끝까지
+
+            PrintArray(array[^4..^1]);  // 끝에서 4번째부터 끝에서 2번째까지
+        }
+    }
+}
